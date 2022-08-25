475bc0dff5f84f6f81fee3c7493de97ea9c9c757
DuMaciel <edumaciel1221@gmail.com>
2022-08-25 18:16:59 -0300

    FixRoom

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,10 +2,33 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 const checkButtons = document.querySelectorAll(".actions a.check")
 checkButtons.forEach(button =>{
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
+const deleteButton = document.querySelectorAll(".actions a.delete")
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+    event.preventDefault()
+
+    const form = document.querySelector(".modal form")
+
+    const roomid = document.querySelector("#room-id").dataset.id
+    const slug = check ? "check" : "delete"
+
+    form.setAttribute("action", `/room/${roomid}/:question/${slug}`)
+
+    modalTitle.innerHTML = check ? "Marcar como lido": "Excluir"
+    modalDescription.innerHTML = check ? "Tem certeza que deseja marcar como lida esta pergunta?" : "Tem certeza que deseja excluir esta pergunta?"
+    modalButton.innerHTML = check ? "Sim, marcar" : "Sim, excluir"
+    check ? modalButton.classList.remove("red") :  modalButton.classList.add("red") 
+    modal.open()
+}
