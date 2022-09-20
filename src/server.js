08959b2a91e25df7025cbbe95ddd843a5d778265
DuMaciel <edumaciel1221@gmail.com>
2022-09-20 17:09:36 -0300

    FixPort

--- src/server.js
+++ src/server.js
@@ -4,14 +4,16 @@ const path = require("path");
 
 const server = express();
 
+const PORT = process.env.PORT || 3000;
+
 server.set("view engine", "ejs");
 
 server.use(express.static("public"));
 
 server.set("views", path.join(__dirname, "views"));
 
-server.use(express.urlencoded({extended: true}))
+server.use(express.urlencoded({ extended: true }))
 
 server.use(route);
 
-server.listen(3000, () => console.log("RODANDO"));
+server.listen(PORT, () => console.log("RODANDO"));
