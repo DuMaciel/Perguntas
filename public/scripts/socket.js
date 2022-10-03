3d6597e1410aaedda9cc57fbdbd556eac62db9a7
DuMaciel <edumaciel1221@gmail.com>
2022-10-03 17:36:59 -0300

    refector: textarea not update to null

--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const socket = io();
 
+    var textArea = document.getElementById("questionText");
+    const roomId = document.getElementById("room-id").dataset.id;
 
-    const roomId = document.getElementById("room-id").dataset.id
+    textArea.addEventListener("keyup", function () {
+        sessionStorage.setItem("autosave", textArea.value);
+    });
+    window.onload = function () {
+        if (sessionStorage.getItem("autosave"))
+            textArea.value = sessionStorage.getItem("autosave");
+    }
+
 
     socket.on(`update question ${roomId}`, () => {
         window.location.reload();
-    })
+    });
 })
+
+
+
+
+
