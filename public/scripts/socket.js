05b36216a5c7eb80f3f0c0caf46218049a1b9edd
DuMaciel <edumaciel1221@gmail.com>
2022-10-04 15:07:55 -0300

    fix: textarea not update to null

--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -5,11 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const roomId = document.getElementById("room-id").dataset.id;
 
     textArea.addEventListener("keyup", function () {
-        sessionStorage.setItem("autosave", textArea.value);
+        sessionStorage.setItem("autosave", roomId + textArea.value);
     });
+
     window.onload = function () {
-        if (sessionStorage.getItem("autosave"))
-            textArea.value = sessionStorage.getItem("autosave");
+        if (sessionStorage.getItem("autosave")) {
+            let texto = sessionStorage.getItem("autosave");
+            if (texto.slice(0, 6) == roomId) {
+                textArea.value = texto.slice(6, texto.length);
+            }
+        } else {
+            sessionStorage.clear();
+        }
     }
 
 
